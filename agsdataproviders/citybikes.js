328d5c8cbe044becdfc9f0732578a29a583c0eec
Nicholas Furness <nfurness@esri.com>
2013-08-01 14:17:23 -0400

    CityBikes sample module implemented. Still needs Timezone handling added for local results.

--- agsdataproviders/citybikes.js
+++ agsdataproviders/citybikes.js
@@ -10,36 +10,67 @@ Object.size = function(obj) {
     return size;
 };
 
+var classificationScheme = {
+	"0": { "min": 0, "max": 0, "label": "No bikes" },
+	"1": { "min": 1, "max": 1, "label": "1 bike" },
+	"few": { "min": 2, "max": 8, "label": "A few bikes" },
+	"plenty": { "min": 9, "max": 10000, "label": "Plenty of bikes" }
+};
+
+var getBikeRange = function(station) {
+	var bikesAvailable = station.attributes.bikes;
+	var classes = [];
+	for (var k in classificationScheme)
+	{
+		classes.push(k);
+	}
+	
+	for (var i=0; i<classes.length; i++)
+	{
+		var className = classes[i];
+		var classRange = classificationScheme[className];
+		var min = classRange.min;
+		var max = classRange.max;
+
+		if (bikesAvailable >= min && bikesAvailable <= max)
+		{
+			station.attributes["bikesClass"] = classRange.label;
+			break;
+		}
+	}
+	if (!station.attributes.hasOwnProperty("bikesClass"))
+	{
+		station.attributes["bikesClass"] = "Woah, that's a lotta bikes!";
+	}
+};
+
 CityBikes = function () {
 	CityBikes.super_.call(this);
-// 	console.log("CityBikes initialized");
+
 	this._isReady = false;
-	
 	this._cachedNetworks = null;
 	this._cacheExpirationTime = new Date();
 	var _cityBikesNetworksURL = "http://api.citybik.es/networks.json";
 	
 	this._networksCacheTime = 30 * 60000;
-	var _bikesCacheTime = 1 * 60000;
+	this._stationCacheTime = 1 * 60000;
 
-	function _cacheInvalid() {
+	function _cacheInvalid(provider) {
 		var now = new Date();
-		return (this._cachedNetworks == null) || (now >= this._cacheExpirationTime);
+		var cacheInvalid = (provider._cachedNetworks == null) || (now >= provider._cacheExpirationTime);
+		return cacheInvalid;
 	}
 
-	var citybikesProvider = this;	
-
-	this._cacheNetworks = function(callback) {
-		if (_cacheInvalid())
+	this._networks = function(callback) {
+		if (_cacheInvalid(this))
 		{
 			// Load the latest list of city services
 			console.log("Caching Networks...");
 			var added = 0;
+			var provider = this;
 			http.get(_cityBikesNetworksURL, 
 					 function(res)
 			{
-				console.log("Got response from citibik.es...");
-
 				res.setEncoding('utf8');
 				var networksJSON = "";
 
@@ -48,7 +79,7 @@ CityBikes = function () {
 				});
 
 				res.on('end', function() {
-					console.log("Caching...");
+					console.log("Caching Networks...");
 
 					var networks = JSON.parse(networksJSON);
 					var nc = {};
@@ -61,17 +92,17 @@ CityBikes = function () {
 						{
 							network.lat = network.lat / 1000000;
 							network.lng = network.lng / 1000000;
-							var networksCacheEntry = {
+							var networkCacheEntry = {
 								"network": network, 
-								"bikes": { 
+								"stations": { 
 										lastReadTime: -1,
 										cacheExpirationTime: new Date(),
-										cachedBikes: []
+										cachedStations: []
 									},
-								"timezone": ""
+								"timezone": null
 							};
 						
-							nc[network.name] = networksCacheEntry;
+							nc[network.name] = networkCacheEntry;
 						
 // 							getCityCacheTimezoneInfo(cityCacheEntry);
 						
@@ -79,10 +110,10 @@ CityBikes = function () {
 						}
 					}
 				
-					citybikesProvider._cacheExpirationTime = new Date();
-					citybikesProvider._cacheExpirationTime.setTime(citybikesProvider._cacheExpirationTime.getTime() + citybikesProvider._networksCacheTime);
+					provider._cacheExpirationTime = new Date();
+					provider._cacheExpirationTime.setTime(provider._cacheExpirationTime.getTime() + provider._networksCacheTime);
 					console.log("Cached " + added + " new networks!");
-					console.log("Cache expires at: " + citybikesProvider._cacheExpirationTime);
+					console.log("Networks cache expires at: " + provider._cacheExpirationTime);
 			
 					callback(nc);
 				});
@@ -90,12 +121,105 @@ CityBikes = function () {
 		}
 		else
 		{
-			callback(citybikesProvider._cachedNetworks);
+			callback(this._cachedNetworks);
 		}
 	};
 
-	this._cacheNetworks(function(cachedNetworks) {
-		citybikesProvider._cachedNetworks = cachedNetworks;
+	this._stationsForNetwork = function(n, callback) {
+		if (n.stations.lastReadTime != -1 &&
+			n.stations.cacheExpirationTime > new Date())
+		{
+			console.log("Returning cached station results for " + n.network.name);
+			callback(n.stations.cachedStations);
+		}
+		else
+		{
+			var cityBikesUrl = n.network.url;
+			var provider = this;
+			http.get(cityBikesUrl, function (res) {
+				res.setEncoding('utf8');
+				var stationsJSON = "";
+			
+				res.on('data', function(chunk) {
+					stationsJSON = stationsJSON + chunk;
+				});
+
+				res.on('end', function() {
+					var stationsData = JSON.parse(stationsJSON);
+
+					n.stations.cachedBikes = [];
+					var minX = 0;
+					var minY = 0;
+					var maxX = 0;
+					var maxY = 0;
+					for (var i=0; i < stationsData.length; i++)
+					{
+						var station = stationsData[i];
+					
+						var tmp = new Date(station.timestamp);
+						// The timestamps are CEST - fix by - 2 hours.
+						tmp.setTime(tmp.getTime() - (2 * 60 * 60 * 1000));
+	// 					console.log(tmp.toString() + " >> " + tmp.toUTCString());
+						var epochMS = new Date(tmp).getTime();
+						var localEpochMS = new Date(epochMS).getTime();
+						station["citybikeTimestamp"] = epochMS;
+	// 					console.log(city);
+	// 					console.log(city.citySvc);
+						if (n.timezone)
+						{
+							var gmtOffset = parseInt(n.timezone.gmtOffset);
+	// 						console.log("Adjusting timezone for " + city.citySvc.name + " by " + gmtOffset);
+							localEpochMS = localEpochMS + (gmtOffset * 1000);
+						}
+						else
+						{
+// 							console.log("Uh oh - no timezone for " + n.network.name);
+						}
+						station["localTimestamp"] = localEpochMS;
+	// 					console.log(epochMS + " >> " + localEpochMS);
+					
+						var stationFeature = { 
+							"geometry": {"spatialReference": {"wkid":4326}},
+							"attributes": {}
+						};
+						var x = station.lng / 1000000;
+						var y = station.lat / 1000000;
+						stationFeature.geometry["x"] = x;
+						stationFeature.geometry["y"] = y;
+						if (x < minX) minX = x;
+						if (x > maxX) maxX = x;
+						if (y < minY) minY = y;
+						if (y > maxY) maxY = y;
+						stationFeature.attributes = JSON.parse(JSON.stringify(station));
+						getBikeRange(stationFeature);
+						delete stationFeature.attributes["lat"];
+						delete stationFeature.attributes["lng"];
+						delete stationFeature.attributes["coordinates"];
+						n.stations.cachedStations.push(stationFeature);
+					}
+					n.stations["extent"] = {
+						"xmin": minX, "ymin": minY,
+						"xmax": maxX, "ymax": maxY
+					};
+					n.stations.lastReadTime = new Date();
+
+					n.stations.cacheExpirationTime =
+						new Date(n.stations.lastReadTime.getTime() + provider._stationCacheTime);
+
+					console.log(util.format('Cached %d stations for %s at %s (expires %s)',
+											stationsData.length, n.network.name,
+											n.stations.lastReadTime,
+											n.stations.cacheExpirationTime));
+				
+					callback(n.stations.cachedStations);
+				});
+			});
+		}
+	};
+
+	var citybikesProvider = this;
+	this._networks(function(networkList) {
+		citybikesProvider._cachedNetworks = networkList;
 		citybikesProvider._isReady = true;
 	});
 };
@@ -140,6 +264,17 @@ Object.defineProperties(CityBikes.prototype, {
 				{"name" : "citybikeTimestamp", "type" : "esriFieldTypeDate", "alias" : "Citybike Timestamp", "length" : 36, "nullable" : "true"},
 				{"name" : "localTimestamp", "type" : "esriFieldTypeDate", "alias" : "Local Timestamp", "length" : 36, "nullable" : "true"}
 			];
+		}
+	},
+	featuresForQuery: {
+		value: function(serviceId, layerId, query, callback) {
+			var provider = this;
+			this._networks(function(networks) {
+				var network = networks[serviceId];
+				provider._stationsForNetwork(network, function(stationFeatures) {
+					callback(stationFeatures);
+				});
+			});
 		}
 	}
 });
