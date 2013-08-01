e37320d43cca579965701d42f76f4e9b9f8d33ce
Nicholas Furness <nfurness@esri.com>
2013-08-01 14:56:06 -0400

    Added timezone code. Still needs some refactoring but this is OK for now.

--- agsdataproviders/citybikes.js
+++ agsdataproviders/citybikes.js
@@ -1,6 +1,7 @@
 var agsdp = require("./agsdataproviderbase");
 var util = require('util');
 var http = require('http');
+var fs = require('fs');
 
 Object.size = function(obj) {
     var size = 0, key;
@@ -8,40 +9,6 @@ Object.size = function(obj) {
         if (obj.hasOwnProperty(key)) size++;
     }
     return size;
-};
-
-var classificationScheme = {
-	"0": { "min": 0, "max": 0, "label": "No bikes" },
-	"1": { "min": 1, "max": 1, "label": "1 bike" },
-	"few": { "min": 2, "max": 8, "label": "A few bikes" },
-	"plenty": { "min": 9, "max": 10000, "label": "Plenty of bikes" }
-};
-
-var getBikeRange = function(station) {
-	var bikesAvailable = station.attributes.bikes;
-	var classes = [];
-	for (var k in classificationScheme)
-	{
-		classes.push(k);
-	}
-	
-	for (var i=0; i<classes.length; i++)
-	{
-		var className = classes[i];
-		var classRange = classificationScheme[className];
-		var min = classRange.min;
-		var max = classRange.max;
-
-		if (bikesAvailable >= min && bikesAvailable <= max)
-		{
-			station.attributes["bikesClass"] = classRange.label;
-			break;
-		}
-	}
-	if (!station.attributes.hasOwnProperty("bikesClass"))
-	{
-		station.attributes["bikesClass"] = "Woah, that's a lotta bikes!";
-	}
 };
 
 CityBikes = function () {
@@ -55,11 +22,79 @@ CityBikes = function () {
 	this._networksCacheTime = 30 * 60000;
 	this._stationCacheTime = 1 * 60000;
 
+	this._networkTimezones = {};
+	this._networksAwaitingTimezone = {};
+
+	var _timezoneCacheFilename = "timezones.json";
+
+	if (fs.existsSync(_timezoneCacheFilename))
+	{
+		this._networkTimezones = JSON.parse(fs.readFileSync(_timezoneCacheFilename, 'utf8'));
+		console.log("Loaded timezones from " + _timezoneCacheFilename);
+	}
+
 	function _cacheInvalid(provider) {
 		var now = new Date();
 		var cacheInvalid = (provider._cachedNetworks == null) || (now >= provider._cacheExpirationTime);
 		return cacheInvalid;
 	}
+
+	this._getTimezone = function(networkCacheEntry) {
+		var network = networkCacheEntry.network;
+		var networkName = networkCacheEntry.network.name;
+		if (this._networkTimezones.hasOwnProperty(networkName))
+		{
+			networkCacheEntry["timezone"] = this._networkTimezones[networkName];
+		}
+		else
+		{
+			this._networksAwaitingTimezone[networkName] = true;
+			var timezoneUrl = util.format("http://api.timezonedb.com/?key=%s&lat=%d&lng=%d&format=json", "IMPMC00M2XNY", network.lat, network.lng);
+			http.get(timezoneUrl, function (res) {
+				var timezoneJSON = "";
+				res.setEncoding('utf8');
+				res.on('data', function(chunk) {
+					timezoneJSON += chunk;
+				});
+				res.on('end', function() {
+					var loadedTimezoneOK = false;
+					var timezone = null;
+					try
+					{
+						timezone = JSON.parse(timezoneJSON);
+						loadedTimezoneOK = true;
+					}
+					catch (err)
+					{
+						console.log(err)
+						console.log(timezoneJSON);
+					}
+				
+					if (loadedTimezoneOK)
+					{
+						if (timezone.status === "OK")
+						{
+							delete timezone["status"];
+							delete timezone["message"];
+							timezone["cacheRefreshDue"] = (new Date()).getTime() + 24*60*60000;
+							networkCacheEntry["timezone"] = timezone;
+
+							_networkTimezones[networkName] = timezone;
+				
+							delete _networksAwaitingTimezone[networkName];
+							console.log("Timezone: " + networkName + " (" + Object.size(_networksAwaitingTimezone) + ")");
+							console.log(timezone);
+							if (Object.size(_networksAwaitingTimezone) == 0)
+							{
+								fs.writeFile(timezoneCacheFilename, JSON.stringify(_networkTimezones));
+								console.log("Wrote timezones to " + timezoneCacheFilename);
+							}
+						}
+					}
+				});
+			});
+		}
+	};
 
 	this._networks = function(callback) {
 		if (_cacheInvalid(this))
@@ -104,7 +139,7 @@ CityBikes = function () {
 						
 							nc[network.name] = networkCacheEntry;
 						
-// 							getCityCacheTimezoneInfo(cityCacheEntry);
+							provider._getTimezone(networkCacheEntry);
 						
 							added++
 						}
@@ -122,6 +157,40 @@ CityBikes = function () {
 		else
 		{
 			callback(this._cachedNetworks);
+		}
+	};
+
+	this._classificationScheme = {
+		"0": { "min": 0, "max": 0, "label": "No bikes" },
+		"1": { "min": 1, "max": 1, "label": "1 bike" },
+		"few": { "min": 2, "max": 8, "label": "A few bikes" },
+		"plenty": { "min": 9, "max": 10000, "label": "Plenty of bikes" }
+	};
+
+	this._getBikeRange = function(station) {
+		var bikesAvailable = station.attributes.bikes;
+		var classes = [];
+		for (var k in this._classificationScheme)
+		{
+			classes.push(k);
+		}
+	
+		for (var i=0; i<classes.length; i++)
+		{
+			var className = classes[i];
+			var classRange = this._classificationScheme[className];
+			var min = classRange.min;
+			var max = classRange.max;
+
+			if (bikesAvailable >= min && bikesAvailable <= max)
+			{
+				station.attributes["bikesClass"] = classRange.label;
+				break;
+			}
+		}
+		if (!station.attributes.hasOwnProperty("bikesClass"))
+		{
+			station.attributes["bikesClass"] = "Woah, that's a lotta bikes!";
 		}
 	};
 
@@ -157,26 +226,23 @@ CityBikes = function () {
 						var station = stationsData[i];
 					
 						var tmp = new Date(station.timestamp);
+
 						// The timestamps are CEST - fix by - 2 hours.
 						tmp.setTime(tmp.getTime() - (2 * 60 * 60 * 1000));
-	// 					console.log(tmp.toString() + " >> " + tmp.toUTCString());
 						var epochMS = new Date(tmp).getTime();
 						var localEpochMS = new Date(epochMS).getTime();
 						station["citybikeTimestamp"] = epochMS;
-	// 					console.log(city);
-	// 					console.log(city.citySvc);
+
 						if (n.timezone)
 						{
 							var gmtOffset = parseInt(n.timezone.gmtOffset);
-	// 						console.log("Adjusting timezone for " + city.citySvc.name + " by " + gmtOffset);
 							localEpochMS = localEpochMS + (gmtOffset * 1000);
 						}
 						else
 						{
-// 							console.log("Uh oh - no timezone for " + n.network.name);
+							console.log("Uh oh - no timezone for " + n.network.name);
 						}
 						station["localTimestamp"] = localEpochMS;
-	// 					console.log(epochMS + " >> " + localEpochMS);
 					
 						var stationFeature = { 
 							"geometry": {"spatialReference": {"wkid":4326}},
@@ -191,7 +257,7 @@ CityBikes = function () {
 						if (y < minY) minY = y;
 						if (y > maxY) maxY = y;
 						stationFeature.attributes = JSON.parse(JSON.stringify(station));
-						getBikeRange(stationFeature);
+						provider._getBikeRange(stationFeature);
 						delete stationFeature.attributes["lat"];
 						delete stationFeature.attributes["lng"];
 						delete stationFeature.attributes["coordinates"];
