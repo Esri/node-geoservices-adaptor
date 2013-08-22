304117ab6d4e0effefde4e85198527bc000937a4
Nicholas Furness <nfurness@esri.com>
2013-08-22 16:16:11 -0400

    Start the cache manager

--- src/cachemanager.js
+++ src/cachemanager.js
@@ -1,6 +1,12 @@
 CacheEntry = function() {
+	this.Expiration = new Date();
+	this.ETag = "";
 }
 
 CacheManager = function() {
+	// A mapping of ETags to cache responses
 	this.cacheLookup = {};
+	
+	this.getEntry = function(url, ifNoneMatch) {
+	}
 }
